a9d52a47cf51cbad8838c14b5d5ab11209e5062a
Rinaldo <contact@numericgraphics.com>
2019-04-04 09:11:01 +0200

    fix middleware

--- src/model/videojs-urn-middleware.js
+++ src/model/videojs-urn-middleware.js
@@ -1,15 +1,25 @@
 import {DataproviderService} from "srgletterbox-web/app/dataProvider/services/DataProviderService";
 
 const videojsUrnMiddleware = function (player) {
-    console.log("PLAYER", player);
     async function setSource(srcObj, next) {
-        console.log("setSource", srcObj);
         this.dataproviderService = new DataproviderService();
-        let mediaComposition = await this.dataproviderService.getMediaCompositionByUrn(srcObj.src).then((result) => {
-            result.json();
-        });
+        let mediaComposition = await this.dataproviderService.getMediaCompositionByUrn(srcObj.src).then(result => result);
+        console.log("perf1");
         let chapter = mediaComposition.chapterList.find( chapter => chapter.urn === mediaComposition.chapterUrn);
-        next(null, chapter.resourceList.map(item => item.url));
+        console.log("perf2");
+        let resourceList = chapter.resourceList.map(item => item.url);
+        let mimes = chapter.resourceList.map(item => item.mimeType);
+        let resources = [];
+        resourceList.forEach(element => {
+            resources.push({
+                src: element
+            });
+        });
+        mimes.forEach((element, i) => {
+            resources[i].type = element;
+        });
+
+        next(null, resources[0]);
     }
 
     return {
--- src/views/videojs-component.js
+++ src/views/videojs-component.js
@@ -3,21 +3,18 @@ import {PlayerBase} from "./libs/playerBase";
 import videojs from '../../node_modules/video.js/dist/video.js'
 import SeekBarCustomComponent from './seekbar-custom-component.js'
 import ProgressControlComponent from './progress-control-component.js'
-import '../../node_modules/video.js/dist/video-js.css'
-import './css/style.scss';
 import videojsUrnMiddleware from "../model/videojs-urn-middleware";
 import {DataproviderService} from "srgletterbox-web/app/dataProvider/services/DataProviderService";
-
-const ProgressControl = videojs.getComponent('ProgressControl');
+import '../../node_modules/video.js/dist/video-js.css'
+import './css/style.scss';
 
 
 class VideoJSComponent extends PlayerBase {
 
     constructor(props) {
         super(props);
-        console.log('VideoJSComponent constructor');
         this.videoNode = React.createRef();
-        this.createVideojs();
+        videojs.use('srgssr/urn', videojsUrnMiddleware);
     }
 
     componentDidMount() {
@@ -42,43 +39,20 @@ class VideoJSComponent extends PlayerBase {
         this.player = videojs(this.videoNode, props, function onPlayerReady() {
             console.log('onPlayerReady', this);
         });
+        this.player.src({type: 'srgssr/urn', src: this.props.urn});
     }
 
     render() {
-        console.log('render');
         return (
             <div data-vjs-player>
                 <video ref={node => this.videoNode = node} width={this.props.width} height={this.props.height}
                        className="video-js vjs-big-play-centered vjs-srgssr-skin" controls>
-                    <source src={this.props.urn} type="srgssr/urn"/>
                 </video>
             </div>
         )
     }
 
     createVideojs() {
-        console.log('createVideojs');
-
-        videojs.use('srgssr/urn', function (player) {
-            console.log("PLAYER", player);
-            async function setSource(srcObj, next) {
-                console.log("setSource", srcObj);
-                // this.dataproviderService = new DataproviderService();
-                // let mediaComposition = await this.dataproviderService.getMediaCompositionByUrn(srcObj.src).then((result) => {
-                //     result.json();
-                // });
-                // let chapter = mediaComposition.chapterList.find( chapter => chapter.urn === mediaComposition.chapterUrn);
-                // next(null, chapter.resourceList.map(item => item.url));
-                next();
-            }
-
-            return {
-                setSource,
-                currentTime(ct) {
-                    return ct;
-                }
-            };
-        });
 
     }
 }
